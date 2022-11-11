3879271cc353e57f7b8cb4e8d74afaf822a27217
Jaecobd1 <jaecob.d1@gmail.com>
2022-11-10 19:38:39 -0500

    first commit

---
+++ app/app.js
@@ -0,0 +1,601 @@
+var LISTS = [{
+        name: 'Groceries',
+        listItems: [{
+                name: 'milk',
+                checked: false,
+                category: 'dairy'
+            },
+            {
+                name: 'cheese',
+                checked: false,
+                category: 'dairy'
+            },
+            {
+                name: 'oranges',
+                checked: false,
+                category: 'fruit'
+            },
+            {
+                name: 'bread',
+                checked: false,
+                category: 'dairy'
+            },
+            {
+                name: 'butter',
+                checked: false,
+                category: 'dairy'
+            },
+            {
+                name: 'bacon',
+                checked: false,
+                category: 'meat'
+            },
+            {
+                name: 'bananas',
+                checked: false,
+                category: 'fruit'
+            },
+            {
+                name: 'Orange Juice',
+                checked: false,
+                category: 'drinks'
+            },
+            {
+                name: 'Gold Fish',
+                checked: false,
+                category: 'crackers'
+            },
+            {
+                name: 'Chips',
+                checked: false,
+                category: 'snacks'
+            }
+        ]
+    },
+    {
+        name: 'Camping',
+        listItems: [{
+                name: 'tent',
+                checked: false,
+                category: 'sporting goods'
+            },
+            {
+                name: 'chairs',
+                checked: false,
+                category: 'sporting goods'
+            },
+            {
+                name: 'bug spray',
+                checked: false,
+                category: 'sporting goods'
+            },
+            {
+                name: 'table',
+                checked: false,
+                category: 'sporting goods'
+            },
+            {
+                name: 'sleeping bag',
+                checked: false,
+                category: 'sporting goods'
+            },
+            {
+                name: 'Eggs',
+                checked: false,
+                category: 'grocery'
+            },
+            {
+                name: 'Lantern',
+                checked: false,
+                category: 'sporting goods'
+            },
+            {
+                name: 'Toilet Paper',
+                checked: false,
+                category: 'bath tissues'
+            },
+            {
+                name: 'Hot dogs',
+                checked: false,
+                category: 'food'
+            },
+            {
+                name: 'Fishing Pole',
+                checked: false,
+                category: 'sporting goods'
+            },
+        ]
+    },
+    {
+        name: 'House Hold',
+        listItems: [{
+                name: 'Tide Pods',
+                checked: false,
+                category: 'laundry'
+            },
+            {
+                name: 'Drier Sheets',
+                checked: false,
+                category: 'laundry'
+            },
+            {
+                name: 'Clorox wipes',
+                checked: false,
+                category: 'cleaning'
+            },
+            {
+                name: 'dishsoap',
+                checked: false,
+                category: 'dish'
+            },
+            {
+                name: 'Trash Bags',
+                checked: false,
+                category: 'cleaning'
+            },
+            {
+                name: 'Bathroom cleaner',
+                checked: false,
+                category: 'bathroom'
+            },
+            {
+                name: 'Toothpaste',
+                checked: false,
+                category: 'bathroom'
+            },
+            {
+                name: 'Paper Towels',
+                checked: false,
+                category: 'cleaning'
+            },
+            {
+                name: 'Window cleaner',
+                checked: false,
+                category: 'cleaning'
+            },
+            {
+                name: 'Sponge',
+                checked: false,
+                category: 'dish'
+            },
+        ]
+    },
+    {
+        name: 'Video Games',
+        listItems: [{
+                name: 'Mario Kart',
+                checked: false,
+                category: 'switch'
+            },
+            {
+                name: 'Call of Duty',
+                checked: false,
+                category: 'playstation'
+            },
+            {
+                name: 'Legend of Zelda',
+                checked: false,
+                category: 'Switch'
+            },
+            {
+                name: 'Cooking Mama',
+                checked: false,
+                category: 'DS'
+            },
+            {
+                name: 'Fortnite',
+                checked: false,
+                category: 'pc'
+            },
+            {
+                name: 'Club Penguin',
+                checked: false,
+                category: 'websites'
+            },
+            {
+                name: 'Cool math games',
+                checked: false,
+                category: 'website'
+            },
+            {
+                name: 'Learn 2 Fly',
+                checked: false,
+                category: 'Web game'
+            },
+            {
+                name: 'Animal Crossing',
+                checked: false,
+                category: 'Switch'
+            },
+            {
+                name: 'Switch Sports',
+                checked: false,
+                category: 'Switch'
+            },
+        ]
+    },
+    {
+        name: 'Christmas',
+        listItems: [{
+                name: 'Air Frier',
+                checked: false,
+                category: 'home'
+            },
+            {
+                name: 'PS5',
+                checked: false,
+                category: 'Video Games'
+            },
+            {
+                name: 'Sony G-Master 16-35mm',
+                checked: false,
+                category: 'camera'
+            },
+            {
+                name: 'Switch Joy Cons',
+                checked: false,
+                category: 'Video Games'
+            },
+            {
+                name: 'Candy',
+                checked: false,
+                category: 'Candy'
+            },
+            {
+                name: 'Ethereum',
+                checked: false,
+                category: 'Cryptocurrencies'
+            },
+            {
+                name: 'Books',
+                checked: false,
+                category: 'books'
+            },
+            {
+                name: 'Ipad',
+                checked: false,
+                category: 'electronics'
+            },
+            {
+                name: 'Roomba',
+                checked: false,
+                category: 'vaccum'
+            },
+            {
+                name: 'Guitar',
+                checked: false,
+                category: 'music'
+            },
+        ]
+    },
+    {
+        name: 'Crypto currencies',
+        listItems: [{
+                name: 'Ethereum',
+                checked: false,
+                category: 'Ethereum'
+            },
+            {
+                name: 'Doge coin',
+                checked: false,
+                category: 'Doge'
+            },
+            {
+                name: 'Bitcoin',
+                checked: false,
+                category: 'bitcoin'
+            },
+            {
+                name: 'Mana',
+                checked: false,
+                category: 'Ethereum'
+            },
+            {
+                name: 'Sushi',
+                checked: false,
+                category: 'Ethereum'
+            },
+            {
+                name: 'Solana',
+                checked: false,
+                category: 'Solana'
+            },
+            {
+                name: 'Avalance',
+                checked: false,
+                category: 'Avalance'
+            },
+            {
+                name: 'Joe',
+                checked: false,
+                category: 'Avalance'
+            },
+            {
+                name: 'Tether',
+                checked: false,
+                category: 'Ethereum'
+            },
+            {
+                name: 'BNB',
+                checked: false,
+                category: 'Binance Smart Chain'
+            }
+        ]
+    },
+    {
+        name: 'Todo',
+        listItems: [{
+                name: 'Finish Homework',
+                checked: false,
+                category: 'School'
+            },
+            {
+                name: 'Make Website look more professional',
+                checked: false,
+                category: 'Work'
+            },
+            {
+                name: 'Haircut',
+                checked: false,
+                category: 'personal'
+            },
+            {
+                name: 'Learn Rust',
+                checked: false,
+                category: 'personal'
+            },
+            {
+                name: 'Learn solidity more',
+                checked: false,
+                category: 'personal'
+            },
+            {
+                name: 'Add sections to map on website',
+                checked: false,
+                category: 'work'
+            },
+            {
+                name: 'Study',
+                checked: false,
+                category: 'school'
+            },
+            {
+                name: 'Make Dinner',
+                checked: false,
+                category: 'personal'
+            },
+            {
+                name: 'Go grocery shopping',
+                checked: false,
+                category: 'personal'
+            },
+            {
+                name: 'Get a new car',
+                checked: false,
+                category: 'personal'
+            },
+        ]
+    },
+    {
+        name: 'Books',
+        listItems: [{
+                name: 'Rich Dad poor Dad',
+                checked: false,
+                category: 'self'
+            },
+            {
+                name: 'The compound effect',
+                checked: false,
+                category: 'self'
+            },
+            {
+                name: 'Atomic Habits',
+                checked: false,
+                category: 'self'
+            },
+            {
+                name: 'The art of Clean Code',
+                checked: false,
+                category: 'knowledge'
+            },
+            {
+                name: 'The entreprenurial rollercoaster',
+                checked: false,
+                category: 'self'
+            },
+            {
+                name: 'Harry Potter',
+                checked: false,
+                category: 'Fiction'
+            },
+            {
+                name: 'Big book of small python projects',
+                checked: false,
+                category: 'knowledge'
+            },
+            {
+                name: 'Steve jobs bio',
+                checked: false,
+                category: 'Biography'
+            },
+            {
+                name: 'Think and grow rich',
+                checked: false,
+                category: 'self'
+            },
+            {
+                name: 'The lean start-up',
+                checked: false,
+                category: 'self'
+            },
+        ]
+    },
+    {
+        name: 'Website planning',
+        listItems: [{
+                name: 'Homepage',
+                checked: false,
+                category: 'page'
+            },
+            {
+                name: 'Nav Bar',
+                checked: false,
+                category: 'component'
+            },
+            {
+                name: 'Footer',
+                checked: false,
+                category: 'component'
+            },
+            {
+                name: 'wrapper',
+                checked: false,
+                category: 'wrapper'
+            },
+            {
+                name: 'Item',
+                checked: false,
+                category: 'Item'
+            },
+            {
+                name: 'Hero',
+                checked: false,
+                category: 'component'
+            },
+            {
+                name: 'About',
+                checked: false,
+                category: 'page'
+            },
+            {
+                name: 'Contact',
+                checked: false,
+                category: 'page'
+            },
+            {
+                name: 'Blog',
+                checked: false,
+                category: 'page'
+            },
+            {
+                name: 'Products',
+                checked: false,
+                category: 'page'
+            },
+        ]
+    },
+    {
+        name: 'Favorite Movies',
+        listItems: [{
+                name: 'Now you see me',
+                checked: false,
+                category: 'magic'
+            },
+            {
+                name: 'Forest Gump',
+                checked: false,
+                category: 'Comedy'
+            },
+            {
+                name: 'The Lion King',
+                checked: false,
+                category: 'Kids'
+            },
+            {
+                name: 'The Prestige',
+                checked: false,
+                category: 'Mystery'
+            },
+            {
+                name: 'Toy Story',
+                checked: false,
+                category: 'Kids'
+            },
+            {
+                name: 'Finding Nemo',
+                checked: false,
+                category: 'Kids'
+            },
+            {
+                name: 'War Dogs',
+                checked: false,
+                category: 'Comedy'
+            },
+            {
+                name: 'Monsters Inc',
+                checked: false,
+                category: 'Kids'
+            },
+            {
+                name: 'Cars',
+                checked: false,
+                category: 'Kids'
+            },
+            {
+                name: 'Black Mirror',
+                checked: false,
+                category: 'Tech'
+            },
+        ]
+    }
+]
+
+function loadLists() {
+    let listString = "<ul>";
+    $.each(LISTS, function(idx, list) {
+        listString += `<li id="${idx}" onclick="loadListItems(${idx})">${list.name} <span class="right">Items: ${list.listItems.length}</span></li>`
+    });
+    listString += "</ul>";
+    $('#app').html(listString);
+}
+
+function itemChecked(element, listIndex, itemIndex) {
+    $(element).parent().toggleClass("strike");
+    let checkedValue = !LISTS[listIndex].listItems[itemIndex].checked;
+    LISTS[listIndex].listItems[itemIndex].checked = checkedValue
+        // console.log(LISTS);
+
+}
+
+function addItem(listIndex) {
+    let newItemName = $("#addItem").val();
+
+    let newItemObj = {
+        name: newItemName,
+        checked: false,
+        category: "",
+    };
+    LISTS[listIndex].listItems.push(newItemObj);
+    console.log(LISTS);
+    loadListItems(listIndex);
+}
+
+function deleteItem(listIndex, idx) {
+    LISTS[listIndex].listItems.splice(idx, 1);
+    loadListItems(listIndex);
+}
+
+function loadListItems(listIndex) {
+    let listString = `<button onclick="loadLists()">Back</button><ul>`;
+
+    $.each(LISTS[listIndex].listItems, function(idx, listItem) {
+        listString += `<li id="{idx}" class="${listItem.checked ? "strike" : ''}"><input ${listItem.checked ? (checked = 'checked') : ''} type="checkbox" id="${idx}" name="${listItem.name}" onclick="itemChecked(this, ${listIndex}, ${idx} )"><span>${listItem.name}</span>
+        <span class="delete" onclick="deleteItem(${listIndex}, ${idx})">
+        Delete
+        </span>
+        </li>`;
+    })
+    listString += `</ul>
+    <div class="addItemInput">
+    <input id="addItem" type="text" placeholder="Item to add...">
+    <button onclick="addItem(${listIndex})">Add</button></div>`;
+    $("#app").html(listString);
+}
+
+function initListeners() {
+
+}
+
+$(document).ready(function() {
+    initListeners();
+})
